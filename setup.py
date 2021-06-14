98f1fce4e944736fb94ff2533a16ba57f58bd766
Wei Ouyang <oeway007@gmail.com>
2021-06-14 21:03:46 +0200

    Generate docs and schema with github actions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf
 setup(
     name="bioimageio.spec",
     version="0.3b",
-    description="Parser library for bioimage model zoo specs",
+    description="Parser and validator library for BioImage.IO specifications",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bioimage-io/spec-bioimage-io",
@@ -34,7 +34,7 @@ setup(
         "typing-extensions",
     ],
     extras_require={"test": ["pytest", "tox"], "dev": ["pre-commit"]},
-    scripts=["scripts/gen_spec_doc.py"],
+    scripts=["scripts/generate_docs.py"],
     project_urls={  # Optional
         "Bug Reports": "https://github.com/bioimage-io/spec-bioimage-io/issues",
         "Source": "https://github.com/bioimage-io/spec-bioimage-io",
