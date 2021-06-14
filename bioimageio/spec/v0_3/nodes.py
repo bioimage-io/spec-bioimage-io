eb3ac297752623eee5838e1d4b64b1f37b59d3c1
fynnbe <thefynnbe@gmail.com>
2021-06-14 12:24:54 +0200

    disable delayed type annotation evaluation
    not needed anymore due to code changes

--- bioimageio/spec/v0_3/nodes.py
+++ bioimageio/spec/v0_3/nodes.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from pathlib import Path
 
 from bioimageio.spec.shared.nodes import ImportedSource
--- bioimageio/spec/v0_3/raw_nodes.py
+++ bioimageio/spec/v0_3/raw_nodes.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import distutils.version
 from dataclasses import dataclass
 from datetime import datetime
