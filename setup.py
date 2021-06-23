32a53d9020025390cd75f21e56e871317a478e1e
k-dominik <k-dominik@users.noreply.github.com>
2021-06-23 07:55:56 +0200

    proper package data include

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     extras_require={"test": ["pytest", "tox", "scikit-learn"], "dev": ["pre-commit"]},
     scripts=["scripts/generate_docs.py"],
-    package_data={'': ['bioimageio/spec/static/licenses.json']},
+    package_data={'bioimageio.spec': ['static/licenses.json']},
     include_package_data=True,
     project_urls={  # Optional
         "Bug Reports": "https://github.com/bioimage-io/spec-bioimage-io/issues",
