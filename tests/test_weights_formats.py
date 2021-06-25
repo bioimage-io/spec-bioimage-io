78f7167a04bbe604ff5807b4d7ddd94b51dc5576
fynnbe <thefynnbe@gmail.com>
2021-06-25 19:09:31 +0200

    fix test_weights_formats
    WeightEntry no longer has weights_format

--- tests/test_weights_formats.py
+++ tests/test_weights_formats.py
@@ -1,8 +1,5 @@
-import dataclasses
-from types import ModuleType
-
+from bioimageio.spec import nodes, raw_nodes, schema
 from bioimageio.spec.shared.common import get_args
-from bioimageio.spec import raw_nodes, schema, nodes
 
 
 def test_weights_formats_have_raw_nodes():
@@ -19,16 +16,3 @@ def test_weights_formats_have_raw_nodes():
     for wf, wecn in zip(weights_formats, weights_entry_class_names):
         comparable = getattr(schema, wecn).weights_format.validate.comparable
         assert comparable == wf, (comparable, wf)
-
-    # every WeightEntry node needs to annotate its corresponding weights_format
-    def check_weights_format_annotation(nodes_module: ModuleType):
-        for wf, wecn in zip(weights_formats, weights_entry_class_names):
-            raw_literal = [f for f in dataclasses.fields(getattr(nodes_module, wecn)) if f.name == "weights_format"][
-                0
-            ].type
-            args = get_args(raw_literal)
-            assert len(args) == 1, args
-            assert args[0] == wf, (args[0], wf)
-
-    check_weights_format_annotation(raw_nodes)
-    check_weights_format_annotation(nodes)
