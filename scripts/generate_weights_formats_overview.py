f5ab98e1758ec7f3091370b1a4dc5dbda004ca8b
fynnbe <thefynnbe@gmail.com>
2021-06-25 19:02:50 +0200

    add check command
    but use generate in pre-commit as it fails on file changes
    and is ready to be commited if using the generate command

--- scripts/generate_weights_formats_overview.py
+++ scripts/generate_weights_formats_overview.py
@@ -1,6 +1,7 @@
 import dataclasses
 import json
 import sys
+from argparse import ArgumentParser
 from pathlib import Path
 
 from bioimageio.spec import download_uri_to_local_path, raw_nodes, schema
@@ -19,7 +20,15 @@ consumer_defaults = {
 }
 
 
-def main():
+def parse_args():
+    p = ArgumentParser(description=("script that generates weights formats overview"))
+    p.add_argument("command", choices=["check", "generate"])
+
+    args = p.parse_args()
+    return args
+
+
+def main(args):
     local = download_uri_to_local_path(MANIFEST_URL)
     with local.open() as f:
         collection = json.load(f)
@@ -27,13 +36,6 @@ def main():
     collection = collection["collections"]
 
     consumers = {c["id"]: c for c in collection}
-
-    def get_weights_format_from_entry_class(entry_class):
-        for field in dataclasses.fields(entry_class):
-            if field.name == "weights_format":
-                return get_args(field.type)[0]
-
-        raise NotImplementedError(entry_class)
 
     weights_format_ids = get_args(WeightsFormat)
     weights_format_class_names = [wf.title().replace("_", "") + "WeightsEntry" for wf in weights_format_ids]
@@ -53,9 +55,19 @@ def main():
 
     overview = {"consumers": consumers, "weight_formats": weights_formats}  # todo: weight_formats -> weights_formats
 
-    with (Path(__file__).parent / "../dist" / "weight_formats_spec.json").open("w") as f:
-        json.dump(overview, f, indent=4, sort_keys=True)
+    if args.command == "generate":
+        with (Path(__file__).parent / "../dist" / "weight_formats_spec.json").open("w") as f:
+            json.dump(overview, f, indent=4, sort_keys=True)
+    elif args.command == "check":
+        with (Path(__file__).parent / "../dist" / "weight_formats_spec.json").open() as f:
+            found = json.load(f)
+
+        if found != overview:
+            return 1
+    else:
+        raise NotImplementedError(args.command)
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    args = parse_args()
+    sys.exit(main(args))
