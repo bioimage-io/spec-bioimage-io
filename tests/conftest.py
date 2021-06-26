59d801f27d192aa7cf4001f34150b01056a5b933
fynnbe <thefynnbe@gmail.com>
2021-06-26 16:21:24 +0200

    remove rf fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,41 +1,15 @@
 import re
 from distutils.version import StrictVersion
 from pathlib import Path
-from typing import Optional
 
 import pytest
 from ruamel.yaml import YAML
 
-from bioimageio.spec import cummulative
 from bioimageio import spec
+from bioimageio.spec import cummulative
 from bioimageio.spec.utils import get_args
 
 yaml = YAML(typ="safe")
-
-
-@pytest.fixture
-def rf_config_path_v0_1():
-    return Path(__file__).parent / "../example_specs/models/sklearn/RandomForestClassifier_v0_1.model.yaml"
-
-
-@pytest.fixture
-def rf_config_path_v0_3_1():
-    return Path(__file__).parent / "../example_specs/models/sklearn/RandomForestClassifier_v0_3_1.model.yaml"
-
-
-@pytest.fixture
-def rf_config_path_v0_3_2():
-    return Path(__file__).parent / "../example_specs/models/sklearn/RandomForestClassifier_v0_3_2.model.yaml"
-
-
-@pytest.fixture
-def rf_config_path_v0_3(rf_config_path_v0_3_2):
-    return rf_config_path_v0_3_2
-
-
-@pytest.fixture
-def rf_config_path(rf_config_path_v0_3_2):
-    return rf_config_path_v0_3_2
 
 
 def get_unet2d_nuclei_broad_path(version: str):
--- tests/test_versions/test_v0_1.py
+++ tests/test_versions/test_v0_1.py
@@ -1,7 +1,7 @@
 from bioimageio.spec.shared import yaml
 
 
-def test_v0_1(unet2d_nuclei_broad_v0_1_path):
+def test_load_v0_1(unet2d_nuclei_broad_v0_1_path):
     from bioimageio.spec.v0_1 import schema
 
     data = yaml.load(unet2d_nuclei_broad_v0_1_path)
