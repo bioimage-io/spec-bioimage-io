ddd2251e142e886cd54adf95feaeb1c5768273f3
fynnbe <thefynnbe@gmail.com>
2021-06-27 22:27:03 +0200

    add ModelLoader for 0.3

--- bioimageio/spec/v0_1/__init__.py
+++ bioimageio/spec/v0_1/__init__.py
@@ -1,16 +1,7 @@
 from bioimageio.spec.shared import fields, get_args as _get_args
-from . import nodes, raw_nodes, schema
+from . import nodes, raw_nodes, schema, utils
 from .raw_nodes import ModelFormatVersion
 
 fields = fields
-
-
-def maybe_convert_model(data):
-    return data
-
-
-def maybe_convert_manifest(data):
-    return data
-
 
 __version__ = _get_args(ModelFormatVersion)[-1]
--- bioimageio/spec/v0_3/utils.py
+++ bioimageio/spec/v0_3/utils.py
@@ -1,83 +1,23 @@
-import os
-import pathlib
-import warnings
-from functools import singledispatch
-from typing import Optional, Sequence, Union
+from typing import Sequence
 
-from marshmallow import ValidationError, missing
+from marshmallow import missing
 
-from bioimageio.spec.shared import BIOIMAGEIO_CACHE_PATH, yaml
+from bioimageio.spec import v0_1
 from bioimageio.spec.shared.common import get_args
-from bioimageio.spec.shared.transformers import download_uri_to_local_path, resolve_raw_node_to_node, resolve_uri
-from . import nodes, raw_nodes, schema
-from .converters import maybe_convert_model
+from . import converters, nodes, raw_nodes, schema
+from ..shared.model_loader_utils import ModelLoaderBase
 
 
-download_uri_node_to_local_path = download_uri_to_local_path
+class ModelLoader(ModelLoaderBase):
+    preceding_model_loader = v0_1.utils.ModelLoader
+    converters = converters
+    schema = schema
+    raw_nodes = raw_nodes
+    nodes = nodes
 
 
-@singledispatch
-def load_raw_model(source) -> raw_nodes.Model:
-    raise TypeError(source)
-
-
-@load_raw_model.register
-def _(source: dict) -> raw_nodes.Model:
-    data = maybe_convert_model(source)
-    tree: raw_nodes.Model = schema.Model().load(data)
-    return tree
-
-
-@load_raw_model.register
-def _(source: os.PathLike) -> raw_nodes.Model:
-    source = pathlib.Path(source)
-
-    if source.suffix not in (".yml", ".yaml"):
-        raise ValidationError(f"invalid suffix {source.suffix} for source {source}")
-    elif source.suffix == ".yml":
-        warnings.warn(
-            "suffix '.yml' is not recommended and will raise a ValidationError in the future. Use '.yaml' instead "
-            "(https://yaml.org/faq.html)"
-        )
-
-    data = yaml.load(source)
-
-    return load_raw_model(data)
-
-
-@load_raw_model.register
-def _(source: str) -> raw_nodes.Model:
-    if pathlib.Path(source).exists():
-        # assume source is file path
-        source = pathlib.Path(source)
-    else:
-        # assume source is uri
-        source = resolve_uri(source, root_path=BIOIMAGEIO_CACHE_PATH)
-
-    return load_raw_model(source)
-
-
-def load_model(source: Union[os.PathLike, str, dict], root_path: Optional[os.PathLike] = None) -> nodes.Model:
-    if root_path is None:
-        if isinstance(source, dict):
-            raise TypeError("Require root_path if source is dict to resolve relative paths")
-
-        if isinstance(source, str):
-            try:  # is source a path as a string?
-                if not pathlib.Path(source).exists():  # only assume source is a path if path exists
-                    raise FileNotFoundError(source)
-            except (TypeError, FileNotFoundError):
-                # assume source is uri
-                source = resolve_uri(source, root_path=BIOIMAGEIO_CACHE_PATH)
-
-        if isinstance(source, os.PathLike):
-            root_path = pathlib.Path(source).parent
-        else:
-            raise TypeError(f"Expected pathlib.Path, os.PathLike, str or dict, but got {type(source)}")
-
-    root_path = pathlib.Path(root_path)
-
-    return resolve_raw_node_to_node(load_raw_model(source), root_path=root_path, nodes_module=nodes)
+load_raw_model = ModelLoader.load_raw_model
+load_model = ModelLoader.load_model
 
 
 def get_nn_instance(
