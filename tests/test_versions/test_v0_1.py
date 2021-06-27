93317bfb1137f7343eb24d5bcff6f6604f91d56b
fynnbe <thefynnbe@gmail.com>
2021-06-27 22:42:33 +0200

    update test_v0_1

--- tests/test_versions/test_v0_1.py
+++ tests/test_versions/test_v0_1.py
@@ -1,10 +1,12 @@
-from bioimageio.spec.shared import yaml
+def test_load_raw_v0_1(unet2d_nuclei_broad_v0_1_path):
+    from bioimageio.spec.v0_1 import load_raw_model
+
+    raw_model = load_raw_model(unet2d_nuclei_broad_v0_1_path)
+    assert (unet2d_nuclei_broad_v0_1_path.parent / raw_model.documentation).exists()
 
 
 def test_load_v0_1(unet2d_nuclei_broad_v0_1_path):
-    from bioimageio.spec.v0_1 import schema
+    from bioimageio.spec.v0_1 import load_model
 
-    data = yaml.load(unet2d_nuclei_broad_v0_1_path)
-    errors = schema.Model().validate(data)
-
-    assert not errors, errors
+    model = load_model(unet2d_nuclei_broad_v0_1_path)
+    assert model.documentation.exists()
