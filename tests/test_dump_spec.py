2eac9dccbe0b1bd7f9219074e5a30f7d6d23b118
fynnbe <thefynnbe@gmail.com>
2021-06-28 12:48:48 +0200

    update test_dump_spec

--- tests/test_dump_spec.py
+++ tests/test_dump_spec.py
@@ -1,21 +1,24 @@
-from bioimageio.spec import load_raw_model, raw_nodes, schema
 from bioimageio.spec.shared import yaml
 
 
-def test_spec_roundtrip(rf_config_path):
-    data = yaml.load(rf_config_path)
+def test_spec_roundtrip(unet2d_nuclei_broad_any_minor_path):
+    from bioimageio.spec import load_raw_model, serialize_raw_model
 
-    raw_model = load_raw_model(rf_config_path)
-    assert isinstance(raw_model, raw_nodes.Model)
+    data = yaml.load(unet2d_nuclei_broad_any_minor_path)
+    # monkeypatch: yaml.load already converts timestamp to datetime.datetime, while we serialize it to ISO 8601
+    if "timestamp" in data:
+        data["timestamp"] = data["timestamp"].isoformat()
 
-    serialized = schema.Model().dump(raw_model)
+    raw_model = load_raw_model(unet2d_nuclei_broad_any_minor_path)
+
+    serialized = serialize_raw_model(raw_model)
     assert isinstance(serialized, dict)
 
-    # yaml.dump(serialized, Path() / "serialized.yml")
+    # from pathlib import Path
+    # from bioimageio.spec import save_raw_model
+    # save_raw_model(raw_model, Path() / "serialized.yml")
 
     assert serialized == data
-
-    assert not schema.Model().validate(serialized)
 
     raw_model_from_serialized = load_raw_model(serialized)
     assert raw_model_from_serialized == raw_model
