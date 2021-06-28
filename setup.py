ae1c71929bed05a4b0626355bcbef18d22c35d3a
fynnbe <thefynnbe@gmail.com>
2021-06-28 16:52:31 +0200

    fix pytorch package name

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "typing-extensions",
     ],
     entry_points={"console_scripts": ["bioimageio = bioimageio.spec:main"]},
-    extras_require={"test": ["pytest", "tox", "pytorch", "numpy"], "dev": ["pre-commit"]},
+    extras_require={"test": ["pytest", "tox", "torch", "numpy"], "dev": ["pre-commit"]},
     scripts=["scripts/generate_docs.py"],
     package_data={"bioimageio.spec": ["static/licenses.json"]},
     include_package_data=True,
