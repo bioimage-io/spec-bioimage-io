1995721ce5917557d5f00def8c2d7536e87583f3
fynnbe <thefynnbe@gmail.com>
2021-07-06 15:49:52 +0200

    implement package cli command

--- bioimageio/spec/__main__.py
+++ bioimageio/spec/__main__.py
@@ -1,11 +1,14 @@
+import shutil
 import warnings
 from pathlib import Path
 from pprint import pprint
+from typing import List, Optional
 
 import typer
 from ruamel.yaml import YAML
 
-from bioimageio.spec import load_raw_node
+from bioimageio.spec import export_package, load_raw_node
+from bioimageio.spec.shared.utils import resolve_uri
 
 yaml = YAML(typ="safe")
 
@@ -13,8 +16,49 @@ app = typer.Typer()  # https://typer.tiangolo.com/
 
 
 @app.command()
-def package(rdf_source: str, auto_convert: bool = False):
-    raise NotImplementedError
+def package(
+    rdf_source: str = typer.Argument(..., help="RDF source as relative file path or URI"),
+    path: Path = typer.Argument(Path() / "{src_name}-package.zip", help="Save package as"),
+    update_format: bool = typer.Option(
+        False,
+        help="Update format version to the latest (might fail even if source adheres to an old format version). "
+        "To inform the format update the source may specify fields of future versions in "
+        "config:future:<future version>.",  # todo: add future documentation
+    ),
+    weights_priority_order: Optional[List[str]] = typer.Option(
+        None,
+        "-wpo",
+        help="For model packages only. "
+        "If given only the first matching weights format present in the model is included. "
+        "Defaults to include all weights present in source.",
+        show_default=False,
+    ),
+):
+    """Package a BioImage.IO resource described by a BioImage.IO Resource Description File (RDF)."""
+    code = _validate(rdf_source, update_format=update_format, update_format_inner=update_format)
+    if code:
+        print(f"Cannot export invalid BioImage.IO RDF {rdf_source}")
+    else:
+        try:
+            tmp_package_path = export_package(
+                rdf_source, update_to_current_format=update_format, weights_priority_order=weights_priority_order
+            )
+        except Exception as e:
+            print(f"Failed to package {rdf_source} due to: {e}")
+            code = 1
+        else:
+            try:
+                rdf_local_source = resolve_uri(rdf_source)
+                path = path.with_name(path.name.format(src_name=rdf_local_source.stem))
+                shutil.move(tmp_package_path, path)
+            except Exception as e:
+                print(f"Failed to move package from {tmp_package_path} to {path} due to: {e}")
+                code = 1
+
+        if not code:
+            print(f"exported bioimageio package from {rdf_source} to {path}")
+
+    raise typer.Exit(code=code)
 
 
 @app.command()
@@ -30,7 +74,7 @@ def validate(
         None, help="For collection RDFs only. Defaults to value of 'update-format'."
     ),
 ):
-    """Validate the BioImage.IO Resource Description File (RDF)"""
+    """Validate a BioImage.IO Resource Description File (RDF)."""
     if update_format_inner is None:
         update_format_inner = update_format
 
