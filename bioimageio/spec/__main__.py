cd79955300ae04770b419baa7ecfdac53ae3746d
fynnbe <thefynnbe@gmail.com>
2021-07-06 11:32:17 +0200

    rename verify -> validate

--- bioimageio/spec/__main__.py
+++ bioimageio/spec/__main__.py
@@ -18,18 +18,18 @@ def package(rdf_source: str, auto_convert: bool = False):
 
 
 @app.command()
-def verify(rdf_source: str, auto_convert: bool = False, auto_convert_inner: bool = None):
+def validate(rdf_source: str, auto_convert: bool = False, auto_convert_inner: bool = None):
     if auto_convert_inner is None:
         auto_convert_inner = auto_convert
 
-    raise typer.Exit(code=_verify(rdf_source, auto_convert, auto_convert_inner))
+    raise typer.Exit(code=_validate(rdf_source, auto_convert, auto_convert_inner))
 
 
-def _verify(rdf_source: str, auto_convert: bool, auto_convert_inner: bool) -> int:
+def _validate(rdf_source: str, auto_convert: bool, auto_convert_inner: bool) -> int:
     try:
         raw_node = load_raw_node(rdf_source, update_to_current_format=auto_convert)
     except Exception as e:
-        print(f"Could not verify {rdf_source}:")
+        print(f"Could not validate {rdf_source}:")
         pprint(e)
         return 1
     else:
@@ -44,21 +44,21 @@ def _verify(rdf_source: str, auto_convert: bool, auto_convert_inner: bool) -> in
                         pprint(e)
                         ret += 1
                     else:
-                        ret += _verify(inner_source, auto_convert_inner, auto_convert_inner)
+                        ret += _validate(inner_source, auto_convert_inner, auto_convert_inner)
 
         return ret
 
 
 @app.command()
 def verify_spec(model_yaml: str, auto_convert: bool = False):
-    warnings.warn("'verify_spec' is deprecated in favor of 'verify'")
-    return verify(model_yaml, auto_convert)
+    warnings.warn("'verify_spec' is deprecated in favor of 'validate'")
+    return validate(model_yaml, auto_convert)
 
 
 @app.command()
 def verify_bioimageio_manifest(manifest_yaml: Path, auto_convert: bool = False):
-    warnings.warn("'verify_bioimageio_manifest' is deprecated in favor of 'verify'")
-    return verify(manifest_yaml.absolute().as_uri(), auto_convert)
+    warnings.warn("'verify_bioimageio_manifest' is deprecated in favor of 'validate'")
+    return validate(manifest_yaml.absolute().as_uri(), auto_convert)
 
 
 if __name__ == "__main__":
