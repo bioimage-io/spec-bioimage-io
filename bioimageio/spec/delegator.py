6728378143e6ab2691b7d0684cadcce18233fd6d
fynnbe <thefynnbe@gmail.com>
2021-07-26 11:34:08 +0200

    overload load_node and load_raw_node to accept zips

--- bioimageio/spec/delegator.py
+++ bioimageio/spec/delegator.py
@@ -3,16 +3,11 @@ from __future__ import annotations
 import os
 import pathlib
 from typing import Dict, Optional, Sequence, TYPE_CHECKING, Tuple, Union
-from zipfile import ZIP_DEFLATED, ZipFile
+from zipfile import ZIP_DEFLATED
 
 from bioimageio.spec.shared import raw_nodes
-from bioimageio.spec.shared.common import (
-    BIOIMAGEIO_CACHE_PATH,
-    get_format_version_module,
-    get_latest_format_version_module,
-)
+from bioimageio.spec.shared.common import get_format_version_module, get_latest_format_version_module
 from bioimageio.spec.shared.io import IO_Interface, Node, RawNode, resolve_rdf_source_and_type
-from bioimageio.spec.shared.utils import resolve_uri
 
 if TYPE_CHECKING:
     import bioimageio.spec.model
@@ -177,38 +172,3 @@ def get_package_content(
     raw_node, root_path = ensure_raw_node(source, root_path, update_to_current_format)
     io_cls = _get_matching_io_class(raw_node.type, raw_node.format_version)
     return io_cls.get_package_content(raw_node, root_path, weights_priority_order=weights_priority_order)
-
-
-def import_package_as_raw_node(
-    source: Union[os.PathLike, str, raw_nodes.URI], update_to_current_format: bool = False
-) -> RawNode:
-    """import a bioimage.io package (a zip file with an rdf.yaml and therein referenced files) as a raw node
-    (e.g. a raw model node).
-    """
-    rdf_path = extract_zip(source)
-    return load_raw_node(rdf_path, update_to_current_format=update_to_current_format)
-
-
-def import_package(source: Union[os.PathLike, str, raw_nodes.URI], update_to_current_format: bool = False) -> Node:
-    """import a bioimage.io package (a zip file with an rdf.yaml and therein referenced files) (e.g. as a model node)"""
-    rdf_path = extract_zip(source)
-    return load_node(rdf_path, update_to_current_format=update_to_current_format)
-
-
-def extract_zip(source: Union[os.PathLike, str, raw_nodes.URI]) -> pathlib.Path:
-    """extract a zip source to BIOIMAGEIO_CACHE_PATH"""
-    local_source = resolve_uri(source)
-    assert isinstance(local_source, pathlib.Path)
-    BIOIMAGEIO_CACHE_PATH.mkdir(exist_ok=True, parents=True)
-    package_path = BIOIMAGEIO_CACHE_PATH / local_source.stem + "_unzipped"
-    with ZipFile(local_source) as zf:
-        zf.extractall(package_path)
-
-    for rdf_name in ["rdf.yaml", "model.yaml", "rdf.yml", "model.yml"]:
-        rdf_path = package_path / rdf_name
-        if rdf_path.exists():
-            break
-    else:
-        raise FileNotFoundError(local_source / "rdf.yaml")
-
-    return rdf_path
--- bioimageio/spec/shared/io.py
+++ bioimageio/spec/shared/io.py
@@ -106,11 +106,10 @@ class IO_Interface(ABC):
 
     @classmethod
     def maybe_convert(cls, data: dict):
-        """
-        If model 'data' is specified in a preceding format this function converts it to the 'current' format of this
-        IO class. Note: In an IO class of a previous format version, this is not the overall latest format version.
-        The 'current' format version is determined by IO_Base.get_matching_io_class() and may be overwritten if
-        'update_to_current_format'--an argument used in several IO methods--is True.
+        """If resource 'data' is specified in a preceding format, convert it to the (newer) format of this IO class.
+
+        Args:
+            data: raw RDF data as loaded by yaml
         """
         raise NotImplementedError
 
@@ -273,12 +272,22 @@ class IO_Base(IO_Interface):
         elif isinstance(raw_node, (str, os.PathLike, raw_nodes.URI)):
             raw_node = resolve_local_uri(raw_node, pathlib.Path())
             if isinstance(raw_node, pathlib.Path):
+                if raw_node.suffix == ".zip":
+                    raw_node = extract_zip(raw_node)
+
                 root_path = raw_node.parent
         else:
             raise TypeError(raw_node)
 
         raw_node = cls.load_raw_node(raw_node)
         return raw_node, root_path
+
+    @classmethod
+    def maybe_convert(cls, data: dict):
+        if cls.preceding_io_class is not None:
+            data = cls.preceding_io_class.maybe_convert(data)
+
+        return cls.converters.maybe_convert(data)
 
     @classmethod
     def load_node(
@@ -351,7 +360,7 @@ class IO_Base(IO_Interface):
         package_content = cls.get_package_content(
             raw_node, root_path=root_path, weights_priority_order=weights_priority_order
         )
-        cls.make_zip(package_path, package_content, compression=compression, compression_level=compression_level)
+        make_zip(package_path, package_content, compression=compression, compression_level=compression_level)
         return package_path
 
     @classmethod
@@ -447,23 +456,45 @@ class IO_Base(IO_Interface):
         package["rdf.yaml"] = cls.serialize_raw_node(raw_node)
         return dict(package)
 
-    @staticmethod
-    def make_zip(
-        path: pathlib.Path, content: Dict[str, Union[str, pathlib.Path]], *, compression: int, compression_level: int
-    ):
-        with ZipFile(path, "w", compression=compression, compresslevel=compression_level) as myzip:
-            for arc_name, file_or_str_content in content.items():
-                if isinstance(file_or_str_content, str):
-                    myzip.writestr(arc_name, file_or_str_content)
-                else:
-                    myzip.write(file_or_str_content, arcname=arc_name)
 
-    @classmethod
-    def maybe_convert(cls, data: dict):
-        if cls.preceding_io_class is not None:
-            data = cls.preceding_io_class.maybe_convert(data)
+def extract_zip(source: Union[os.PathLike, str, raw_nodes.URI]) -> pathlib.Path:
+    """extract a zip source to BIOIMAGEIO_CACHE_PATH"""
+    local_source = resolve_uri(source)
+    assert isinstance(local_source, pathlib.Path)
+    BIOIMAGEIO_CACHE_PATH.mkdir(exist_ok=True, parents=True)
+    package_path = BIOIMAGEIO_CACHE_PATH / local_source.stem + "_unzipped"
+    with ZipFile(local_source) as zf:
+        zf.extractall(package_path)
 
-        return cls.converters.maybe_convert(data)
+    for rdf_name in ["rdf.yaml", "model.yaml", "rdf.yml", "model.yml"]:
+        rdf_path = package_path / rdf_name
+        if rdf_path.exists():
+            break
+    else:
+        raise FileNotFoundError(local_source / "rdf.yaml")
+
+    return rdf_path
+
+
+def make_zip(
+    path: pathlib.Path, content: Dict[str, Union[str, pathlib.Path]], *, compression: int, compression_level: int
+) -> None:
+    """Write a zip archive.
+
+    Args:
+        path: output path to write to.
+        content: dict with archive names and local file paths or strings for text files.
+        compression: The numeric constant of compression method.
+        compression_level: Compression level to use when writing files to the archive.
+                           See https://docs.python.org/3/library/zipfile.html#zipfile.ZipFile
+
+    """
+    with ZipFile(path, "w", compression=compression, compresslevel=compression_level) as myzip:
+        for arc_name, file_or_str_content in content.items():
+            if isinstance(file_or_str_content, str):
+                myzip.writestr(arc_name, file_or_str_content)
+            else:
+                myzip.write(file_or_str_content, arcname=arc_name)
 
 
 def resolve_rdf_source_and_type(source: Union[os.PathLike, str, dict, raw_nodes.URI]) -> Tuple[dict, str]:
@@ -471,6 +502,9 @@ def resolve_rdf_source_and_type(source: Union[os.PathLike, str, dict, raw_nodes.
         data = source
     else:
         source = resolve_local_uri(source, pathlib.Path())
+        if source.suffix == ".zip":
+            source = extract_zip(source)
+
         data, root_path = get_dict_and_root_path_from_yaml_source(source)
 
     type_ = data.get("type", "model")  # todo: remove default 'model' type
