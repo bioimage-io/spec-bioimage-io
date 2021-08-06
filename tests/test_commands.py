0a15989a1369a20282f7a6f9be7c0863d340707e
fynnbe <thefynnbe@gmail.com>
2021-08-06 16:47:58 +0200

    simplify test_validate_invalid_input

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,7 +1,4 @@
 from copy import copy
-from tempfile import TemporaryFile
-
-from bioimageio.spec.shared import yaml
 
 
 def test_validate_valid_model(unet2d_nuclei_broad_any):
@@ -16,7 +13,5 @@ def test_validate_invalid_model(unet2d_nuclei_broad_latest):
 
     data = copy(unet2d_nuclei_broad_latest)
     del data["test_inputs"]  # invalidate data
-    with TemporaryFile(mode="w+") as f:
-        yaml.dump(data, f)
-        assert validate(f, update_format=True, update_format_inner=False) == 1
-        assert validate(f, update_format=False, update_format_inner=False) == 1
+    assert validate(data, update_format=True, update_format_inner=False) == 1
+    assert validate(data, update_format=False, update_format_inner=False) == 1
