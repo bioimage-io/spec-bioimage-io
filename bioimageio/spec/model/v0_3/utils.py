179bc40d614d5dfb500283350caa296ecd33e4f2
fynnbe <thefynnbe@gmail.com>
2021-09-06 15:56:43 +0200

    only filter weights if weights_priority_order is given

--- bioimageio/spec/model/v0_3/utils.py
+++ bioimageio/spec/model/v0_3/utils.py
@@ -9,11 +9,12 @@ def filter_resource_description(
 ) -> raw_nodes.Model:
     # filter weights
     raw_rd = deepcopy(raw_rd)
-    for wfp in weights_priority_order or []:
-        if wfp in raw_rd.weights:
-            raw_rd.weights = {wfp: raw_rd.weights[wfp]}
-            break
-    else:
-        raise ValueError(f"Not found any of the specified weights formats ({weights_priority_order})")
+    if weights_priority_order is not None:
+        for wfp in weights_priority_order:
+            if wfp in raw_rd.weights:
+                raw_rd.weights = {wfp: raw_rd.weights[wfp]}
+                break
+        else:
+            raise ValueError(f"Not found any of the specified weights formats ({weights_priority_order})")
 
     return raw_rd
