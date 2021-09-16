0b1bc254dbea78d836e9460e1a388ac4e5ab57a2
fynnbe <thefynnbe@gmail.com>
2021-09-16 13:14:35 +0200

    parametrize pre- and postprocessing tests

--- tests/test_schemas_processing.py
+++ tests/test_schemas_processing.py
@@ -1,19 +1,32 @@
+from typing import Union
+
 import pytest
 from marshmallow import ValidationError
 from pytest import raises
 
 
-class TestPreprocessing:
-    class TestZeroMeanUniVarianceKwargs:
-        def test_invalid(self):
-            from bioimageio.spec.model import schema
+pre_and_post_processing = [
+    (True, "zero_mean_unit_variance", {"mode": "fixed", "mean": 1, "std": 2, "axes": "xy"}),
+    (False, "zero_mean_unit_variance", {"mode": "unknown", "mean": 1, "std": 2, "axes": "xy"}),
+]
 
-            with raises(ValidationError):
-                schema.Preprocessing().load({"name": "zero_mean_unit_variance"})
 
-        def test_mode_fixed(self):
-            from bioimageio.spec.model import schema
+@pytest.fixture(
+    params=[("Preprocessing", valid, name, kwargs) for valid, name, kwargs in pre_and_post_processing + []]
+    + [("Postprocessing", valid, name, kwargs) for valid, name, kwargs in pre_and_post_processing + []]
+)
+def processing_test_data(request):
+    return request.param
 
-            schema.Preprocessing().load(
-                {"name": "zero_mean_unit_variance", "kwargs": {"mode": "fixed", "mean": 1, "std": 2, "axes": "xy"}}
-            )
+
+def test_processing(processing_test_data):
+    proc_class_name, valid, name, kwargs = processing_test_data
+    from bioimageio.spec.model import schema
+
+    proc: Union[schema.Preprocessing, schema.Postprocessing] = getattr(schema, proc_class_name)()
+
+    if valid:
+        proc.load({"name": name, "kwargs": kwargs})
+    else:
+        with raises(ValidationError):
+            proc.load({"name": name, "kwargs": kwargs})
