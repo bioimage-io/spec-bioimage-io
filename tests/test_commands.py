b01f1d39d44c2315f780be5358f81ea64efedc61
fynnbe <thefynnbe@gmail.com>
2021-09-24 13:05:14 +0200

    update test_commands

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -4,8 +4,8 @@ from copy import copy
 def test_validate_valid_model(unet2d_nuclei_broad_any):
     from bioimageio.spec.commands import validate
 
-    assert validate(unet2d_nuclei_broad_any, update_format=True, update_format_inner=False) == 0
-    assert validate(unet2d_nuclei_broad_any, update_format=False, update_format_inner=False) == 0
+    assert not validate(unet2d_nuclei_broad_any, update_format=True, update_format_inner=False)
+    assert not validate(unet2d_nuclei_broad_any, update_format=False, update_format_inner=False)
 
 
 def test_validate_invalid_model(unet2d_nuclei_broad_latest):
@@ -13,5 +13,5 @@ def test_validate_invalid_model(unet2d_nuclei_broad_latest):
 
     data = copy(unet2d_nuclei_broad_latest)
     del data["test_inputs"]  # invalidate data
-    assert validate(data, update_format=True, update_format_inner=False) == 1
-    assert validate(data, update_format=False, update_format_inner=False) == 1
+    assert validate(data, update_format=True, update_format_inner=False)
+    assert validate(data, update_format=False, update_format_inner=False)
