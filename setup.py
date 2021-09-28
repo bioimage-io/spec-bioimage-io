b8a17eee54aa291501bcdf6f1b5977259129b4aa
Dominik Kutra <k-dominik@users.noreply.github.com>
2021-09-28 12:18:27 +0200

    force encoding to utf-8
    read_text takes the default locale... Now fun fact - when packaging ilastik and starting via double click, this seems to default to ascii

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_namespace_packages, setup
 ROOT_DIR = Path(__file__).parent.resolve()
 long_description = (ROOT_DIR / "README.md").read_text(encoding="utf-8")
 VERSION_FILE = ROOT_DIR / "bioimageio" / "spec" / "VERSION"
-VERSION = json.loads(VERSION_FILE.read_text())["version"]
+VERSION = json.loads(VERSION_FILE.read_text(encoding="utf-8"))["version"]
 
 
 setup(
