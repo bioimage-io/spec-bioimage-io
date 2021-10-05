711f40a79b0140043307ba51591a885cf576d0c9
fynnbe <thefynnbe@gmail.com>
2021-10-05 17:59:29 +0200

    add asserts to debug ci errors
    tests pass locally (win)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,16 +1,19 @@
+import pathlib
 import subprocess
 import zipfile
 
 
 def test_cli_validate_model(unet2d_nuclei_broad_latest_path):
+    assert unet2d_nuclei_broad_latest_path.exists()
     ret = subprocess.run(["bioimageio", "validate", str(unet2d_nuclei_broad_latest_path)])
     assert ret.returncode == 0
 
 
 def test_cli_validate_model_package(unet2d_nuclei_broad_latest_path, tmpdir):
-    zf_path = tmpdir / "package.zip"
+    zf_path = pathlib.Path(tmpdir / "package.zip")
     with zipfile.ZipFile(zf_path, "w") as zf:
         zf.write(unet2d_nuclei_broad_latest_path, "rdf.yaml")
 
+    assert zf_path.exists()
     ret = subprocess.run(["bioimageio", "validate", str(zf_path)])
     assert ret.returncode == 0
