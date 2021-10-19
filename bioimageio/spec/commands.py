cca969c88aba87c03efb30015f3d50150aac43cf
fynnbe <thefynnbe@gmail.com>
2021-10-19 21:21:39 +0200

    account for bad source

--- bioimageio/spec/commands.py
+++ bioimageio/spec/commands.py
@@ -34,37 +34,47 @@ def validate(
     if update_format_inner is None:
         update_format_inner = update_format
 
-    rdf_source, source_name, root = resolve_rdf_source(rdf_source)
-    if not isinstance(rdf_source, dict):
-        raise TypeError(f"expected loaded resource to be a dictionary, but got type {type(dict)}: {rdf_source}")
-
     error = None
     tb = None
-    raw_rd = None
+    nested_errors: Dict[str, list] = {}
     try:
-        raw_rd = load_raw_resource_description(rdf_source, update_to_current_format=update_format)
-    except ValidationError as e:
-        error = nested_default_dict_as_nested_dict(e.normalized_messages())
+        rdf_source, source_name, root = resolve_rdf_source(rdf_source)
     except Exception as e:
         error = str(e)
         tb = traceback.format_tb(e.__traceback__)
+        try:
+            source_name = str(rdf_source)
+        except Exception as e:
+            source_name = str(e)
 
-    nested_errors: Dict[str, list] = {}
-    if raw_rd is not None and raw_rd.type == "collection":
-        for inner_category in KNOWN_COLLECTION_CATEGORIES:
-            for inner in getattr(raw_rd, inner_category) or []:
-                try:
-                    inner_source = inner.source
-                except Exception as e:
-                    inner_summary = {"error": str(e)}
-                else:
-                    inner_summary = validate(inner_source, update_format_inner, update_format_inner)
+    if not isinstance(rdf_source, dict):
+        error = f"expected loaded resource to be a dictionary, but got type {type(rdf_source)}: {str(rdf_source)}"
 
-                if inner_summary["error"] is not None:
-                    nested_errors[inner_category] = nested_errors.get(inner_category, []) + [inner_summary]
+    raw_rd = None
+    if error is None:
+        try:
+            raw_rd = load_raw_resource_description(rdf_source, update_to_current_format=update_format)
+        except ValidationError as e:
+            error = nested_default_dict_as_nested_dict(e.normalized_messages())
+        except Exception as e:
+            error = str(e)
+            tb = traceback.format_tb(e.__traceback__)
 
-        if nested_errors:
-            error = f"Errors in collections of {list(nested_errors)}"
+        if raw_rd is not None and raw_rd.type == "collection":
+            for inner_category in KNOWN_COLLECTION_CATEGORIES:
+                for inner in getattr(raw_rd, inner_category) or []:
+                    try:
+                        inner_source = inner.source
+                    except Exception as e:
+                        inner_summary = {"error": str(e)}
+                    else:
+                        inner_summary = validate(inner_source, update_format_inner, update_format_inner)
+
+                    if inner_summary["error"] is not None:
+                        nested_errors[inner_category] = nested_errors.get(inner_category, []) + [inner_summary]
+
+            if nested_errors:
+                error = f"Errors in collections of {list(nested_errors)}"
 
     return {
         "name": source_name if raw_rd is None else raw_rd.name,
