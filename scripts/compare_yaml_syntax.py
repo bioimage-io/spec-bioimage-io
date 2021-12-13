0522ffc7517a52df545e8281575438b64ab5bf2c
fynnbe <thefynnbe@gmail.com>
2021-12-13 13:49:45 +0100

    update compare_yaml_syntax script
    (script is only for reference  why we use ruamel.yaml)

--- scripts/compare_yaml_syntax.py
+++ scripts/compare_yaml_syntax.py
@@ -1,23 +1,37 @@
-import json
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
 
-from marshmallow_jsonschema import JSONSchema
+try:
+    from ruamel.yaml import YAML
 
-import bioimageio.spec
-
-from bioimageio.spec.shared.common import ruamel_yaml, pyyaml_yaml
-
-if ruamel_yaml is None:
+    ruamel_yaml = YAML(typ="safe")
+except ImportError:
     raise RuntimeError("Cannot compare yaml syntax without the ruamel.yaml package")
+
+try:
+    import yaml as _pyyaml_yaml
+
+    class pyyaml_yaml:
+        @staticmethod
+        def load(path: Path):
+            with path.open() as f:
+                return _pyyaml_yaml.load(f)
+
+
+except ImportError:
+    raise RuntimeError("Cannot compare yaml syntax without the pyyaml package")
 
 
 def parse_args():
     p = ArgumentParser(
         description="Check for differences between yaml 1.1 (using PyYAML) and yaml 1.2 syntax (using ruamel.yaml)."
     )
-    p.add_argument("resource_description_path", type=Path)
+    p.add_argument(
+        "--resource_description_path",
+        type=Path,
+        default=Path(__file__).parent / "../example_specs/models/unet2d_nuclei_broad/rdf.yaml",
+    )
     args = p.parse_args()
     return args
 
