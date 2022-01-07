bbc279ba2a465de0b704cb0e828cf6107776bbb7
fynnbe <thefynnbe@gmail.com>
2022-01-07 15:14:44 +0100

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -52,6 +52,11 @@ def invalid_rdf_v0_4_0_duplicate_tensor_names(unet2d_nuclei_broad_base_path):
 
 
 @pytest.fixture
+def unet2d_nuclei_broad_collection():
+    return yaml.load(pathlib.Path(__file__).parent / "../example_specs/collections/unet2d_nuclei_broad_coll/rdf.yaml")
+
+
+@pytest.fixture
 def unet2d_nuclei_broad_url():
     return "https://raw.githubusercontent.com/bioimage-io/spec-bioimage-io/main/example_specs/models/unet2d_nuclei_broad/rdf.yaml"
 
