2d64d6bab82b19287375e4babcdcac4364275add
fynnbe <thefynnbe@gmail.com>
2022-01-12 20:53:31 +0100

    fix colleciton converter

--- bioimageio/spec/collection/v0_2/converters.py
+++ bioimageio/spec/collection/v0_2/converters.py
@@ -1,7 +1,5 @@
 from typing import Any, Dict
 
-from bioimageio.spec.rdf.v0_2.converters import maybe_convert as maybe_convert_rdf
-
 
 def maybe_convert(data: Dict[str, Any]) -> Dict[str, Any]:
     if data.get("format_version") in ("0.2.0", "0.2.1"):
@@ -21,4 +19,4 @@ def maybe_convert(data: Dict[str, Any]) -> Dict[str, Any]:
 
         data["format_version"] = "0.2.2"
 
-    return maybe_convert_rdf(data)
+    return data
