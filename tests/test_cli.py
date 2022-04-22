f382dacc02040741d3c1d36afa976a3de09022ab
fynnbe <thefynnbe@gmail.com>
2022-04-22 23:54:43 +0200

    use test fixture unet2d_nuclei_broad_latest as path

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,18 +1,23 @@
 import os
 import subprocess
 import zipfile
+from typing import Sequence
 
 from bioimageio.spec.io_ import load_raw_resource_description, save_raw_resource_description
 from bioimageio.spec.shared import yaml
 
 
-def test_cli_validate_model(unet2d_nuclei_broad_latest_path):
-    ret = subprocess.run(["bioimageio", "validate", str(unet2d_nuclei_broad_latest_path)])
+def run_subprocess(commands: Sequence[str], **kwargs) -> subprocess.CompletedProcess:
+    return subprocess.run(commands, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8", **kwargs)
+
+
+def test_cli_validate_model(unet2d_nuclei_broad_latest):
+    ret = run_subprocess(["bioimageio", "validate", str(unet2d_nuclei_broad_latest)])
     assert ret.returncode == 0
 
 
 def test_cli_validate_model_url():
-    ret = subprocess.run(
+    ret = run_subprocess(
         [
             "bioimageio",
             "validate",
@@ -25,7 +30,7 @@ def test_cli_validate_model_url():
 def test_cli_validate_model_url_wo_cache():
     env = os.environ.copy()
     env["BIOIMAGEIO_USE_CACHE"] = "false"
-    ret = subprocess.run(
+    ret = run_subprocess(
         [
             "bioimageio",
             "validate",
@@ -37,28 +42,28 @@ def test_cli_validate_model_url_wo_cache():
 
 
 def test_cli_validate_model_doi():
-    ret = subprocess.run(["bioimageio", "validate", "10.5281/zenodo.5744489"])
+    ret = run_subprocess(["bioimageio", "validate", "10.5281/zenodo.5744489"])
     assert ret.returncode == 0
 
 
-def test_cli_validate_model_package(unet2d_nuclei_broad_latest_path, tmpdir):
+def test_cli_validate_model_package(unet2d_nuclei_broad_latest, tmpdir):
     zf_path = tmpdir / "package.zip"
     with zipfile.ZipFile(zf_path, "w") as zf:
-        zf.write(unet2d_nuclei_broad_latest_path, "rdf.yaml")
+        zf.write(unet2d_nuclei_broad_latest, "rdf.yaml")
 
-    ret = subprocess.run(["bioimageio", "validate", str(zf_path)])
+    ret = run_subprocess(["bioimageio", "validate", str(zf_path)])
     assert ret.returncode == 0
 
 
-def test_cli_validate_model_package_wo_cache(unet2d_nuclei_broad_latest_path, tmpdir):
+def test_cli_validate_model_package_wo_cache(unet2d_nuclei_broad_latest, tmpdir):
     env = os.environ.copy()
     env["BIOIMAGEIO_USE_CACHE"] = "false"
 
     zf_path = tmpdir / "package.zip"
     with zipfile.ZipFile(zf_path, "w") as zf:
-        zf.write(unet2d_nuclei_broad_latest_path, "rdf.yaml")
+        zf.write(unet2d_nuclei_broad_latest, "rdf.yaml")
 
-    ret = subprocess.run(["bioimageio", "validate", str(zf_path)], env=env)
+    ret = run_subprocess(["bioimageio", "validate", str(zf_path)], env=env)
     assert ret.returncode == 0
 
 
@@ -67,7 +72,7 @@ def test_cli_update_format(unet2d_nuclei_broad_before_latest, tmp_path):
     save_raw_resource_description(load_raw_resource_description(unet2d_nuclei_broad_before_latest), in_path)
     assert in_path.exists()
     path = tmp_path / "rdf_new.yaml"
-    ret = subprocess.run(["bioimageio", "update-format", str(in_path), str(path)])
+    ret = run_subprocess(["bioimageio", "update-format", str(in_path), str(path)])
     assert ret.returncode == 0
     assert path.exists()
 
@@ -78,7 +83,7 @@ def test_update_rdf(unet2d_nuclei_broad_base_path, tmp_path):
     update_path = tmp_path / "update.yaml"
     yaml.dump(dict(name="updated", outputs=[{"name": "updated", "halo": ["KEEP", "DROP", 0, 9, 9]}]), update_path)
     out_path = tmp_path / "output.yaml"
-    ret = subprocess.run(["bioimageio", "update-rdf", str(in_path), str(update_path), str(out_path)])
+    ret = run_subprocess(["bioimageio", "update-rdf", str(in_path), str(update_path), str(out_path)])
     assert ret.returncode == 0
     actual = yaml.load(out_path)
     assert actual["name"] == "updated"
