7dcad7ed1ce1cdaf9f44fc457ee8119f9f939833
fynnbe <thefynnbe@gmail.com>
2022-04-27 14:02:19 +0200

    add test_load_animal_nickname

--- tests/test_io.py
+++ tests/test_io.py
@@ -6,3 +6,14 @@ def test_get_resource_package_content(unet2d_nuclei_broad_latest, unet2d_nuclei_
     local_keys = set(from_local_content)
     remote_keys = set(from_remote_content)
     assert local_keys == remote_keys
+
+
+def test_load_animal_nickname():
+    from bioimageio.spec import load_raw_resource_description
+    from bioimageio.spec.model.v0_4.raw_nodes import Model as Model04
+
+    nickname = "impartial-shrimp"
+    model = load_raw_resource_description(nickname)
+    assert isinstance(model, Model04)
+    assert model.format_version.split(".")[:2] == (0, 4)
+    assert model.config["bioimageio"]["nickname"] == nickname
