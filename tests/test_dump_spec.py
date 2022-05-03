933dc80d6619d4ece7d13fbe99167fbdbc7e077b
fynnbe <thefynnbe@gmail.com>
2022-05-03 10:43:01 +0200

    add test_serialize_with_link_in_root

--- tests/test_dump_spec.py
+++ tests/test_dump_spec.py
@@ -78,3 +78,24 @@ def test_serialize_with_link_in_path(dataset_rdf, tmp_path: Path):
 
     serialized = serialize_raw_resource_description_to_dict(data, convert_absolute_paths=True)
     assert serialized["documentation"] == "docs.md"
+
+
+@pytest.mark.skipif(platform.system() == "Windows")
+def test_serialize_with_link_in_root(dataset_rdf, tmp_path: Path):
+    from bioimageio.spec import load_raw_resource_description, serialize_raw_resource_description_to_dict
+
+    data = load_raw_resource_description(dataset_rdf)
+
+    true_root = tmp_path / "root"
+    true_root.mkdir()
+    linked_root = tmp_path / "link"
+    linked_root.symlink_to(true_root, target_is_directory=True)
+
+    doc_path = true_root / "docs.md"
+    doc_path.write_text("# Documentation")
+
+    data.root_path = linked_root  # root path is symlink to true root
+    data.documentation = doc_path
+
+    serialized = serialize_raw_resource_description_to_dict(data, convert_absolute_paths=True)
+    assert serialized["documentation"] == "docs.md"
