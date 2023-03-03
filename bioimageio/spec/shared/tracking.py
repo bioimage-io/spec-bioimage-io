f12afd8f2f81276171849c713b1e62981877cb21
fynnbe <thefynnbe@gmail.com>
2023-03-03 09:52:28 +0100

    turn test main block into cli

--- bioimageio/spec/shared/tracking.py
+++ bioimageio/spec/shared/tracking.py
@@ -5,16 +5,26 @@ def report_resource_download(resource_id: str):
     import requests  # not available in pyodide
 
     uadata = f'&uadata={{"brands":[{{"brand":"{CONSUMER_ID}","version":"{CONSUMER_VERSION or "unknown"}"}}]}}'
-    url = f"https://bioimage.matomo.cloud/matomo.php?download=https://doi.org/{resource_id}&idsite=1&rec=1&r=646242&h=13&m=35&s=20&url=http://bioimage.io/#/?id={resource_id}{uadata}"
+    url = (
+        f"https://bioimage.matomo.cloud/matomo.php?download=https://doi.org/{resource_id}&idsite=1&rec=1"
+        f"&r=646242&h=13&m=35&s=20&url=http://bioimage.io/#/?id={resource_id}{uadata}"
+    )
 
     r = requests.get(url)
     r.raise_for_status()
 
 
-if __name__ == "__main__":
-    model_doi = "test3"
+def main(model_doi: str = "test3"):
     report_resource_download(model_doi)
-    # summary is not real-time
     print(
-        f"https://bioimage.matomo.cloud/?module=API&method=Actions.getDownload&downloadUrl=https://doi.org/{model_doi}&idSite=1&idCustomReport=1&period=year&date=2023-03-01&format=JSON&token_auth=anonymous"
+        "report available at:\n"
+        f"https://bioimage.matomo.cloud/?module=API&method=Actions.getDownload&downloadUrl=https://doi.org/{model_doi}"
+        "&idSite=1&idCustomReport=1&period=year&date=2023-03-01&format=JSON&token_auth=anonymous\n"
+        "note: reporting is not real-time"
     )
+
+
+if __name__ == "__main__":
+    import typer
+
+    typer.run(main)
