f8d3b7702758814069e2362dfa59b3f37311c1d5
fynnbe <thefynnbe@gmail.com>
2023-03-03 11:03:53 +0100

    remove tracking cli

--- bioimageio/spec/shared/tracking.py
+++ bioimageio/spec/shared/tracking.py
@@ -12,20 +12,3 @@ def report_resource_download(resource_id: str):
 
     r = requests.get(url)
     r.raise_for_status()
-
-
-def main(model_doi: str = "test3"):
-    print(f"reporting download of '{model_doi}' ...")
-    report_resource_download(model_doi)
-    print(
-        "report available at:\n"
-        f"https://bioimage.matomo.cloud/?module=API&method=Actions.getDownload&downloadUrl=https://doi.org/{model_doi}"
-        "&idSite=1&idCustomReport=1&period=year&date=2023-03-01&format=JSON&token_auth=anonymous\n"
-        "note: reporting is not real-time"
-    )
-
-
-if __name__ == "__main__":
-    import typer
-
-    typer.run(main)
