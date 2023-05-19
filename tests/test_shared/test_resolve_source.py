00ab794c30c3d9cb2ba3a2c0a2e7d8f88edb4bd9
fynnbe <thefynnbe@gmail.com>
2023-05-19 09:06:02 +0200

    mock _url_download for exact assert

--- tests/test_shared/test_resolve_source.py
+++ tests/test_shared/test_resolve_source.py
@@ -1,23 +1,32 @@
-import pytest
+import os
 from pathlib import Path
+from typing import Optional
+
+import pytest
 
 from bioimageio.spec.shared.raw_nodes import URI
+
+
+def mock_download(uri: URI, output: Optional[os.PathLike] = None, pbar=None):
+    return Path(__file__).resolve()
 
 
 @pytest.mark.parametrize(
     "src",
     [
-        "https://raw.githubusercontent.com/bioimage-io/spec-bioimage-io/main/example_specs/models/unet2d_nuclei_broad/rdf.yaml",
+        "https://example.com/fake",
         Path(__file__),
         __file__,
-        URI(
-            "https://raw.githubusercontent.com/bioimage-io/spec-bioimage-io/main/example_specs/models/unet2d_nuclei_broad/rdf.yaml"
-        ),
+        URI("https://example.com/fake"),
     ],
 )
 def test_get_resolved_source_path(src):
     from bioimageio.spec.shared import get_resolved_source_path
 
+    import bioimageio.spec
+
+    bioimageio.spec.shared._resolve_source._download_url = mock_download
     res = get_resolved_source_path(src, root_path=Path(__file__).parent)
     assert isinstance(res, Path)
     assert res.exists()
+    assert res == Path(__file__).resolve()
