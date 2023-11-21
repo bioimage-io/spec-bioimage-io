dc80b128443222c63ac4f03e02fc07af70d50acc
fynnbe <thefynnbe@gmail.com>
2023-11-21 02:18:37 +0100

    add common submodule

---
+++ bioimageio/spec/common.py
@@ -0,0 +1,7 @@
+from bioimageio.spec._internal.base_nodes import FileDescr as FileDescr
+from bioimageio.spec._internal.base_nodes import InvalidDescription as InvalidDescription
+from bioimageio.spec._internal.types import BioimageioYamlContent as BioimageioYamlContent
+from bioimageio.spec._internal.types import FileName as FileName
+from bioimageio.spec._internal.types import FileSource as FileSource
+from bioimageio.spec._internal.types import Sha256 as Sha256
+from bioimageio.spec._internal.types import YamlValue as YamlValue
