0f9f1572532b04874a79c2ccd78caa054b273066
fynnbe <thefynnbe@gmail.com>
2024-02-26 16:10:46 +0100

    transfer test_load_non_existing_rdf  from core

---
+++ tests/test_io.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+import pytest
+
+
+def test_load_non_existing_rdf():
+    from bioimageio.core import load_description
+
+    spec_path = Path("some/none/existing/path/to/spec.model.yaml")
+
+    with pytest.raises(FileNotFoundError):
+        _ = load_description(spec_path)
