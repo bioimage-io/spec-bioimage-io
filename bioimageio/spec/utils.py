08931448ca977560937bfd51fc6826161ee92382
fynnbe <thefynnbe@gmail.com>
2024-04-26 11:44:05 +0200

    expose get_sha256

--- bioimageio/spec/utils.py
+++ bioimageio/spec/utils.py
@@ -1,5 +1,6 @@
 from ._internal.io import download as download
 from ._internal.io import extract_file_name as extract_file_name
+from ._internal.io import get_sha256 as get_sha256
 from ._internal.io import (
     identify_bioimageio_yaml_file_name as identify_bioimageio_yaml_file_name,
 )
