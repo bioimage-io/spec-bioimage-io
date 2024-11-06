ef7e63d78df90b76ecfe79715575e14048b0ee67
fynnbe <thefynnbe@gmail.com>
2024-11-06 23:32:54 +0100

    improve documentation of generated submodule __init__.py files

--- bioimageio/spec/dataset/__init__.py
+++ bioimageio/spec/dataset/__init__.py
@@ -4,14 +4,17 @@ implementaions of all released minor versions are available in submodules:
 - dataset v0_2: `bioimageio.spec.dataset.v0_2.DatasetDescr`
 - dataset v0_3: `bioimageio.spec.dataset.v0_3.DatasetDescr`
 """
+
 from typing import Union
 
 from pydantic import Discriminator
 from typing_extensions import Annotated
 
-from .v0_2 import DatasetDescr as DatasetDescr_v0_2
-from .v0_3 import DatasetDescr as DatasetDescr
-from .v0_3 import DatasetDescr as DatasetDescr_v0_3
+from . import v0_2, v0_3
+
+DatasetDescr = v0_3.DatasetDescr
+DatasetDescr_v0_2 = v0_2.DatasetDescr
+DatasetDescr_v0_3 = v0_3.DatasetDescr
 
 AnyDatasetDescr = Annotated[
     Union[DatasetDescr_v0_2, DatasetDescr_v0_3], Discriminator("format_version")
