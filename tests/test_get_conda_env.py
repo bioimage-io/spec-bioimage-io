408b0a88424047f53e0f7414897939e4766da1d6
fynnbe <thefynnbe@gmail.com>
2024-12-09 13:56:16 +0100

    add pytorch 1.5.1 pins

--- tests/test_get_conda_env.py
+++ tests/test_get_conda_env.py
@@ -85,6 +85,7 @@ def test_get_default_pytorch_env():
 
     versions: Dict[str, List[Optional[str]]] = {
         "pytorch": [
+            "1.5.1",
             "1.6.0",
             "1.7.0",
             "1.7.1",
