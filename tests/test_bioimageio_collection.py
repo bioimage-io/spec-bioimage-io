12e076095400446fd35b0a5b046685e3079b371e
fynnbe <thefynnbe@gmail.com>
2025-03-26 12:44:42 +0100

    fix impartial-shrimp test

--- tests/test_bioimageio_collection.py
+++ tests/test_bioimageio_collection.py
@@ -17,7 +17,7 @@ EXCLUDE_FIELDS_FROM_ROUNDTRIP: Mapping[str, Collection[str]] = {
     "affable-shark/1.1": {"inputs"},  # preprocessing assert_dtype added
     "charismatic-whale/1.0.1": {"inputs", "outputs"},  # int -> float
     "dynamic-t-rex/1": {"inputs"},  # int -> float
-    "impartial-shrimp/1.1": {"inputs"},  # preprocessing assert_dtype added
+    "impartial-shrimp/1.1": {"inputs", "cite"},  # preprocessing assert_dtype added
     "philosophical-panda/0.0.11": {"outputs"},  # int -> float
     "philosophical-panda/0.1.0": {"outputs"},  # int -> float
     "polite-pig/1.1": {"inputs", "outputs"},
