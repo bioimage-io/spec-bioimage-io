37e1122d8a6c29e4faf193e8a5aea363401b627b
fynnbe <thefynnbe@gmail.com>
2025-05-09 13:07:39 +0200

    add test_progress

---
+++ tests/test_internal/test_progress.py
@@ -0,0 +1,33 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bioimageio.spec._internal.progress import Progressbar
+
+
+def _test_progress_impl(pbar: Progressbar):
+    pbar.total = 10
+    pbar.set_description("round 1")
+    for _ in range(3):
+        pbar.update(2)
+
+    pbar.reset()
+    pbar.set_description("round 2")
+    pbar.total = 2
+    for _ in range(2):
+        pbar.update(1)
+
+    pbar.close()
+
+
+def test_progress_tqdm():
+    from tqdm import tqdm
+
+    _test_progress_impl(tqdm())
+
+
+def test_progress_rich():
+    from bioimageio.spec._internal.progress import RichOverallProgress
+
+    pbar_class = RichOverallProgress()
+    pbar = pbar_class()
+    _test_progress_impl(pbar)
