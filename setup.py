564cbc6287befd505097a3be4d1399206542ab0d
fynnbe <thefynnbe@gmail.com>
2025-06-13 14:38:39 +0200

    bump pyright

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ _ = setup(
                 "jupyter",
                 "pdoc",
                 "pre-commit",
-                "pyright==1.1.401",
+                "pyright==1.1.402",
                 "ruff",
             ]
         ),
