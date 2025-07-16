019d6842e9e6da30ba2105970dd5e1258cd4796b
fynnbe <thefynnbe@gmail.com>
2025-07-16 11:49:45 +0200

    show source url in error when sha is wrong

--- bioimageio/spec/_internal/io.py
+++ bioimageio/spec/_internal/io.py
@@ -705,18 +705,23 @@ def _open_url(
         or hashlib.sha256(str(source).encode(encoding="utf-8")).hexdigest()
     )
 
-    return BytesReader(
-        cache.fetch(
+    try:
+        reader = cache.fetch(
             source,
             fetcher=partial(_fetch_url, progressbar=progressbar),
             force_refetch=digest,
-        ),
-        suffix=source_path.suffix,
-        sha256=sha,
-        original_file_name=source_path.name,
-        original_root=source.parent,
-        is_zipfile=None,
-    )
+        )
+    except Exception as e:
+        raise ValueError(f"Failed to fetch {source}.") from e
+    else:
+        return BytesReader(
+            reader,
+            suffix=source_path.suffix,
+            sha256=sha,
+            original_file_name=source_path.name,
+            original_root=source.parent,
+            is_zipfile=None,
+        )
 
 
 def _fetch_url(
