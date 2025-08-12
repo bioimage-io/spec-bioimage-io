50b25ff38275f9ba0ba560c116bed85ef1bc74f1
fynnbe <thefynnbe@gmail.com>
2025-08-12 10:03:20 +0200

    bump genericache

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ _ = setup(
     install_requires=[
         "annotated-types>=0.5.0,<1",
         "email_validator",
-        "genericache==0.4.1",
+        "genericache==0.5.1",
         "imageio",
         "loguru",
         "markdown",
