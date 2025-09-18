caf4cd34522a49c1bff44e403689617d27495113
fynnbe <thefynnbe@gmail.com>
2025-09-18 14:41:00 +0200

    include json roundtrip in test_json_schema_is_up_to_date()

--- tests/test_internal/test_json_schema.py
+++ tests/test_internal/test_json_schema.py
@@ -1,3 +1,5 @@
+import json
+
 from deepdiff.diff import DeepDiff
 
 
@@ -6,6 +8,9 @@ def test_json_schema_is_up_to_date():
     from bioimageio.spec.utils import get_bioimageio_json_schema
 
     generated = generate_json_schema()
+    # encode and decode with json for exact match, e.g. json turns int into float
+    generated_json_roundtrip = json.loads(json.dumps(generated))
+
     existing = get_bioimageio_json_schema()
-    diff = DeepDiff(existing, generated)
+    diff = DeepDiff(existing, generated_json_roundtrip)
     assert not diff, diff.pretty()
