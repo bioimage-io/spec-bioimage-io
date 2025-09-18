2d647f998f2d7936e9fc40a63bec2f236d285a64
fynnbe <thefynnbe@gmail.com>
2025-09-18 15:29:20 +0200

    only test json schema in dev env

--- tests/test_internal/test_json_schema.py
+++ tests/test_internal/test_json_schema.py
@@ -2,7 +2,10 @@ import json
 
 from deepdiff.diff import DeepDiff
 
+from tests.utils import expensive_test
 
+
+@expensive_test  # test only in developer environment (success is pydantic version dependent)
 def test_json_schema_is_up_to_date():
     from bioimageio.spec._internal.json_schema import generate_json_schema
     from bioimageio.spec.utils import get_bioimageio_json_schema
