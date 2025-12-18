3f0c6a6d43556584f406867e64450ff7f6105391
fynnbe <thefynnbe@gmail.com>
2025-12-18 10:27:52 +0100

    clean up test_json_schema_is_up_to_date

--- tests/test_internal/test_json_schema.py
+++ tests/test_internal/test_json_schema.py
@@ -1,5 +1,3 @@
-import json
-
 from deepdiff.diff import DeepDiff
 
 from tests.utils import expensive_test
@@ -11,10 +9,6 @@ def test_json_schema_is_up_to_date():
     from bioimageio.spec.utils import get_bioimageio_json_schema
 
     generated = generate_json_schema()
-    # encode and decode with json for exact match, e.g. json turns int into float
-    # generated_json_roundtrip = json.loads(json.dumps(generated))
-    generated_json_roundtrip = generated  # not needed anymore??
-
     existing = get_bioimageio_json_schema()
-    diff = DeepDiff(existing, generated_json_roundtrip)
+    diff = DeepDiff(existing, generated)
     assert not diff, diff.pretty()
