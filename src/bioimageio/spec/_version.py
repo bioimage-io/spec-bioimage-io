6dcbfaaf38140dc5ae5b7d846f3726d7187c9777
fynnbe <thefynnbe@gmail.com>
2026-01-29 16:28:24 +0100

    clarify MAJOR.MINOR.PATCH.LIB format

--- src/bioimageio/spec/_version.py
+++ src/bioimageio/spec/_version.py
@@ -1,1 +1,7 @@
 VERSION = "0.5.7.0"
+"""bioimageio.spec version as MAJOR.MINOR.PATCH.LIB
+
+MAJOR.MINOR.PATCH correspond to the latest model description format version implemented.
+
+LIB may be bumped for library changes that do not affect the format version.
+"""
