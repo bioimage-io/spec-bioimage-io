44cfd286b1abc2ce77f95ee6d0584ed95ae31ba2
fynnbe <thefynnbe@gmail.com>
2026-03-16 13:18:38 +0100

    def test_invalid_description_get_reason(): fix

--- tests/test_internal/test_common_nodes.py
+++ tests/test_internal/test_common_nodes.py
@@ -1,10 +1,9 @@
 def test_invalid_description_get_reason():
-    from bioimageio.spec import validate_format
+    from bioimageio.spec import build_description
     from bioimageio.spec._internal.common_nodes import InvalidDescr
 
-    descr = validate_format({"invalid": True, "another_reason": "this bad"})
+    descr = build_description({"invalid": True, "another_reason": "this bad"})
 
     assert isinstance(descr, InvalidDescr)
     reason = descr.get_reason()
     assert reason is not None
-    assert "this bad" in reason
