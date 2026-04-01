bfb8e70a058d6a0f765ab6c3b9958706135f178a
fynnbe <thefynnbe@gmail.com>
2026-04-01 14:04:01 +0200

    update known_files docstring

--- src/bioimageio/spec/_internal/validation_context.py
+++ src/bioimageio/spec/_internal/validation_context.py
@@ -39,7 +39,21 @@ class ValidationContextBase:
             Callable[[], Dict[str, Optional[Sha256]]], dict
         )
     )
-    """Allows to bypass download and hashing of referenced files."""
+    """Allows to bypass download and hashing of referenced files
+    (even if perform_io_checks is True).
+
+    Keys should be file paths or URL strings as they appear in the
+    bioimageio.yaml file.
+
+    Values are Sha256 values compared to hash values in the description.
+    For `None` values no hash value comparison is performed.
+
+    If `perfrom_io_checks` is True, checked files will be added to
+    this dictionary with their SHA-256 value.
+
+    If `perform_io_checks` is False and `known_files` is not empty,
+    missing, 'unknown' file references are considered invalid.
+    """
 
     update_hashes: bool = False
     """Overwrite specified file hashes with values computed from the referenced file (instead of comparing them).
