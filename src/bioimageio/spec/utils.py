15e436d8695d1bd5518e08c36dd7c7054d55498b
fynnbe <thefynnbe@gmail.com>
2026-08-06 16:37:06 +0200

    implement load_image from zarr

--- src/bioimageio/spec/utils.py
+++ src/bioimageio/spec/utils.py
@@ -4,7 +4,10 @@ from __future__ import annotations
 
 import json
 import shutil
-from typing import TYPE_CHECKING, Any, TypedDict
+from collections.abc import Sequence
+from dataclasses import dataclass
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Literal, TypedDict, overload
 
 from imageio.v3 import imread  # pyright: ignore[reportUnknownVariableType]
 from loguru import logger
@@ -32,6 +35,7 @@ from ._internal.io_utils import write_yaml as write_yaml
 from ._internal.type_guards import is_ndarray
 from ._internal.types import PermissiveFileSource, RelativeFilePath
 from ._internal.utils import files
+from ._utils_zarr import open_zarr_multiscale_array
 
 if TYPE_CHECKING:
     import dask.array
@@ -74,33 +78,97 @@ def get_bioimageio_json_schema() -> dict[str, Any]:
         return json.load(f)
 
 
+@dataclass
+class ImageMeta:
+    axes: Sequence[str] | None = None
+
+
+@overload
 def load_image(
     source: FileDescr | ZipPath | PermissiveFileSource,
-) -> NDArray[Any] | dask.array.Array:
+    *,
+    return_image_meta: Literal[False] = False,
+) -> NDArray[Any] | dask.array.Array: ...
+
+
+@overload
+def load_image(
+    source: FileDescr | ZipPath | PermissiveFileSource,
+    *,
+    return_image_meta: Literal[True],
+) -> tuple[NDArray[Any] | dask.array.Array, ImageMeta]: ...
+
+
+def load_image(
+    source: FileDescr | ZipPath | PermissiveFileSource,
+    *,
+    return_image_meta: bool = False,
+) -> (
+    NDArray[Any] | dask.array.Array | tuple[NDArray[Any] | dask.array.Array, ImageMeta]
+):
     """load a single image as numpy array
 
     Args:
         source: image source
     """
 
+    source = _interprete_file_source(source)
+
+    if source.suffix == ".npy":
+        image = load_array(source)
+        meta = ImageMeta(axes=None)
+    else:
+        try:
+            reader = get_reader(source)
+            image = imread(reader.read(), extension=source.suffix)
+        except Exception:
+            image, meta = _load_zarr_image(source)
+        else:
+            meta = ImageMeta(axes=None)
+            assert is_ndarray(image)
+
+    if return_image_meta:
+        return image, meta
+    else:
+        return image
+
+
+def _load_zarr_image(
+    source: FileDescr | PermissiveFileSource,
+) -> tuple[NDArray[Any] | dask.array.Array, ImageMeta]:
+    """load a single image as numpy array
+
+    Args:
+        source: image source
+    """
+    import dask.array as da
+
+    source = _interprete_file_source(source)
+    if isinstance(source, FileDescr):
+        source = source.source.absolute()
+
+    if isinstance(source, ZipPath):
+        raise NotImplementedError(
+            "Loading zarr images from zip files is not implemented."
+        )
+
+    array, ms = open_zarr_multiscale_array(
+        source.as_posix() if isinstance(source, Path) else str(source)
+    )
+    image = da.from_zarr(array)
+    axes = tuple(str(a) for a in ms.axes())
+    return image, ImageMeta(axes=axes)
+
+
+def _interprete_file_source(source: FileDescr | ZipPath | PermissiveFileSource):
     if isinstance(source, (FileDescr, ZipPath)):
         parsed_source = source
     else:
         parsed_source = interprete_file_source(source)
+        if isinstance(parsed_source, RelativeFilePath):
+            parsed_source = parsed_source.absolute()
 
-    if isinstance(parsed_source, RelativeFilePath):
-        parsed_source = parsed_source.absolute()
-
-    if parsed_source.suffix == ".npy":
-        image = load_array(parsed_source)
-    else:
-        reader = get_reader(parsed_source)
-        image = imread(  # pyright: ignore[reportUnknownVariableType]
-            reader.read(), extension=parsed_source.suffix
-        )
-
-    assert is_ndarray(image)
-    return image
+    return parsed_source
 
 
 def empty_cache():
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_license_zenodo():
     """test that all licenses are known or not known by zenodo.
     Run scripts/update_spdx_licenses_zenodo.py to fix this test"""
@@ -5,3 +8,20 @@ def test_license_zenodo():
 
     for lic in get_spdx_licenses()["licenses"]:
         assert isinstance(lic["isKnownByZenodo"], bool), lic["licenseId"]
+
+
+def test_load_image_zarr():
+    source = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0062A/6001240.zarr/2"
+    from bioimageio.spec.utils import load_image
+
+    img = load_image(source)
+    assert img.shape
+
+
+def test_load_image_zarr_group():
+    source = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0062A/6001240.zarr"
+
+    from bioimageio.spec.utils import load_image
+
+    with pytest.raises(ValueError):
+        _ = load_image(source)
