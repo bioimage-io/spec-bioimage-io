feded5116488c77deb289e5dcf33d103fd55c8c3
fynnbe <thefynnbe@gmail.com>
2026-09-11 11:09:19 +0200

    improve type guards

--- src/bioimageio/spec/_internal/type_guards.py
+++ src/bioimageio/spec/_internal/type_guards.py
@@ -4,16 +4,18 @@ They widen the type to T[Any], which is not always correct."""
 from __future__ import annotations
 
 from collections.abc import Mapping, Sequence
-from typing import Any, TypeVar
+from typing import Any, Hashable, TypeVar
 
 import numpy as np
 from numpy.typing import NDArray
 from typing_extensions import TypeGuard
 
 T = TypeVar("T")
+K = TypeVar("K", bound=Hashable)
+V = TypeVar("V")
 
 
-def is_dict(v: Any) -> TypeGuard[dict[Any, Any]]:
+def is_dict(v: Any | Mapping[K, V]) -> TypeGuard[dict[K, V]]:
     """to avoid Dict[Unknown, Unknown]"""
     return isinstance(v, dict)
 
@@ -30,17 +32,17 @@ def is_kwargs(v: Any | Mapping[Any, T]) -> TypeGuard[Mapping[str, T]]:
     )
 
 
-def is_mapping(v: Any) -> TypeGuard[Mapping[Any, Any]]:
+def is_mapping(v: Any | Mapping[K, V]) -> TypeGuard[Mapping[K, V]]:
     """to avoid Mapping[Unknown, Unknown]"""
     return isinstance(v, Mapping)
 
 
-def is_sequence(v: Any) -> TypeGuard[Sequence[Any]]:
+def is_sequence(v: Any | Sequence[T]) -> TypeGuard[Sequence[T]]:
     """to avoid Sequence[Unknown]"""
     return isinstance(v, Sequence)
 
 
-def is_tuple(v: Any) -> TypeGuard[tuple[Any, ...]]:
+def is_tuple(v: Any | Sequence[T]) -> TypeGuard[tuple[T, ...]]:
     """to avoid Tuple[Unknown, ...]"""
     return isinstance(v, tuple)
 
